27c34d59195807c2ae6a22be386c980ca58f041c
Ghattasss <mohamedfr320@gmail.com>
2023-09-03 18:18:10 +0300

    adding the card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:note_app/views/notes_view.dart';
 
 void main() {
@@ -11,7 +12,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        fontFamily: GoogleFonts.poppins().fontFamily,
         brightness: Brightness.dark,
       ),
       home: NotesView(),
