4eb5bd82c227eb3dc3d1c18e87e2eeb0c74af946
Ghattasss <mohamedfr320@gmail.com>
2023-09-03 18:19:48 +0300

    create card widgwt

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:note_app/views/widgets/coustom_appbar.dart';
+import 'package:note_app/views/widgets/coustom_card_note.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
@@ -21,64 +22,5 @@ class NotesViewBody extends StatelessWidget {
         ],
       ),
     );
-  }
-}
-
-class NoteCard extends StatelessWidget {
-  const NoteCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(15),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              offset: Offset(0, 2),
-              blurRadius: 10,
-            ),
-          ],
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Note Title',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16, bottom: 8),
-                child: Text(
-                  'Note Subtitle',
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.6),
-                    fontSize: 18,
-                  ),
-                ),
-              ),
-              trailing: IconButton(
-                color: Colors.black,
-                iconSize: 26,
-                icon: const Icon(Icons.delete),
-                onPressed: () {},
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Text('may 20, 2021',
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.6),
-                    fontSize: 16,
-                  )),
-            ),
-          ],
-        ));
   }
 }
