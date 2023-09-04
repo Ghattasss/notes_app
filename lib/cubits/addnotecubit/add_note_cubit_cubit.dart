4775a5115a235fac0824ab7bccc29d3a6eb27529
Ghattasss <mohamedfr320@gmail.com>
2023-09-04 23:53:18 +0300

    update add note cubit

--- lib/cubits/addnotecubit/add_note_cubit_cubit.dart
+++
@@ -1,7 +0,0 @@
-import 'package:bloc/bloc.dart';
-
-part 'add_note_cubit_state.dart';
-
-class AddNoteCubitCubit extends Cubit<AddNoteCubitState> {
-  AddNoteCubitCubit() : super(AddNoteCubitInitial());
-}
