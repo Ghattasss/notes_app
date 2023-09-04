558280dd17afcbdabbd6ce2db46d747bc5cef350
Ghattasss <mohamedfr320@gmail.com>
2023-09-04 17:56:40 +0300

    create edit note view

--- lib/views/widgets/coustom_card_note.dart
+++ lib/views/widgets/coustom_card_note.dart
@@ -1,53 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/edit_note_view.dart';
 
 class CoustomNoteCard extends StatelessWidget {
   const CoustomNoteCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          color: const Color(0xffFFCC80),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Text(
-                'Note Title',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-              ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16, bottom: 8),
-                child: Text(
-                  'Note Subtitle',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const EditNoteView(),
+          ),
+        );
+      },
+      child: Container(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          decoration: BoxDecoration(
+            color: const Color(0xffFFCC80),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Text(
+                  'Note Title',
                   style: TextStyle(
-                    color: Colors.black.withOpacity(0.6),
-                    fontSize: 18,
+                    color: Colors.black,
+                    fontSize: 26,
                   ),
                 ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 8),
+                  child: Text(
+                    'Note Subtitle',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.6),
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                  color: Colors.black,
+                  iconSize: 26,
+                  icon: const Icon(Icons.delete),
+                  onPressed: () {},
+                ),
               ),
-              trailing: IconButton(
-                color: Colors.black,
-                iconSize: 26,
-                icon: const Icon(Icons.delete),
-                onPressed: () {},
+              Padding(
+                padding: const EdgeInsets.only(right: 16),
+                child: Text('may 20, 2021',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(0.6),
+                      fontSize: 16,
+                    )),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 16),
-              child: Text('may 20, 2021',
-                  style: TextStyle(
-                    color: Colors.black.withOpacity(0.6),
-                    fontSize: 16,
-                  )),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
