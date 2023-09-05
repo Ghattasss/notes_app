a54465bbe7dd5bbda1a824be307de4fd3ec16b05
Ghattasss <mohamedfr320@gmail.com>
2023-09-05 15:32:11 +0300

    colors list view 2

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -45,7 +45,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
           const SizedBox(
             height: 20,
           ),
-          const ColorsListView(),
+          ColorsListView(),
           const SizedBox(height: 32),
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
--- lib/views/widgets/colors_List_view.dart
+++ lib/views/widgets/colors_List_view.dart
@@ -1,32 +1,65 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
-
+  const ColorItem({super.key, required this.ischoosen, required this.color});
+  final bool ischoosen;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.red,
-      radius: 38,
-    );
+    return ischoosen
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 38,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 34,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 38,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
 
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentindex = 0;
+
+  List<Color> colors = [
+    const Color(0xffAC3931),
+    const Color(0xffE5D352),
+    const Color(0xffD9E76C),
+    const Color(0xff537D8D),
+    const Color(0xff482C3D),
+  ];
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.builder(
-        padding: EdgeInsets.zero,
         scrollDirection: Axis.horizontal,
-        itemCount: 4,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4),
-            child: ColorItem(),
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  currentindex = index;
+                });
+              },
+              child: ColorItem(
+                ischoosen: currentindex == index ? true : false,
+                color: colors[index],
+              ),
+            ),
           );
         },
       ),
