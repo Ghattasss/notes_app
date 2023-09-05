64550badf30272f48f2367258635fa9333e54930
Ghattasss <mohamedfr320@gmail.com>
2023-09-05 13:02:26 +0300

    create get notes method

--- lib/cubits/notescubit/notes_cubit.dart
+++ lib/cubits/notescubit/notes_cubit.dart
@@ -1,8 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:note_app/constans.dart';
 import 'package:note_app/models/note_model.dart';
 
 part 'notes_state.dart';
 
 class NotesCubit extends Cubit<NotesState> {
   NotesCubit() : super(NotesInitial());
+
+  void getNotes() {
+    try {
+      final notes = Hive.box<NoteModel>(kNoteSBox).values.toList();
+      emit(NotesLoaded(notes));
+    } catch (e) {
+      emit(NotesError(e.toString()));
+    }
+  }
 }
--- lib/cubits/notescubit/notes_state.dart
+++ lib/cubits/notescubit/notes_state.dart
@@ -4,8 +4,6 @@ abstract class NotesState {}
 
 final class NotesInitial extends NotesState {}
 
-final class NotesLoading extends NotesState {}
-
 final class NotesLoaded extends NotesState {
   final List<NoteModel> notes;
   NotesLoaded(this.notes);
