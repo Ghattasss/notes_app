63169935bb94c157670f4cc2825d1ad28875e9fa
Ghattasss <mohamedfr320@gmail.com>
2023-09-05 12:54:50 +0300

    create notes state

---
+++ lib/cubits/notescubit/notes_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:note_app/models/note_model.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+}
